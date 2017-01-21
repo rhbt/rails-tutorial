38aecab04c5ad6e7042da7846077fb5dde473947
Ryan Ho <rhbt.dev@gmail.com>
2017-01-21 22:04:08 +0000

    Precompile assets

--- config/application.rb
+++ config/application.rb
@@ -24,5 +24,6 @@ module SampleApp
     config.active_record.raise_in_transactional_callbacks = true
     # Include the authenticity token in remote forms.
     config.action_view.embed_authenticity_token_in_remote_forms = true
+    config.assets.initialize_on_precompile = false
   end
 end
